f37a85d922affb7eaea3d23116e466b0b46cd4ab
Ville Viklund <ville.viklund@geniem.com>
2021-05-10 11:15:26 +0300

    Add admin rights

--- lib/Roles.php
+++ lib/Roles.php
@@ -16,28 +16,44 @@ class Roles implements \TMS\Theme\Base\Interfaces\Controller {
      * Hooks
      */
     public function hooks() : void {
-        add_filter(
-            'map_meta_cap',
-            \Closure::fromCallable( [ $this, 'add_unfiltered_html_capability' ] ),
-            1,
-            3
-        );
+        $this->modify_administrator_caps();
     }
 
     /**
-     * Enable unfiltered_html capability for Editors and Administrators.
-     *
-     * @param array  $caps    The user's capabilities.
-     * @param string $cap     Capability name.
-     * @param int    $user_id The user ID.
-     *
-     * @return array  $caps    The user's capabilities, with 'unfiltered_html' potentially added.
+     * Modify 'administrator' capabilities
      */
-    protected function add_unfiltered_html_capability( $caps, $cap, $user_id ) {
-        if ( $cap === 'unfiltered_html' && user_can( $user_id, 'administrator' ) ) {
-            $caps = [ 'unfiltered_html' ];
+    public function modify_administrator_caps() : void {
+        $admin_rights = [
+            // Site settings
+            'edit_site_setting'              => true,
+            'read_site_setting'              => true,
+            'delete_site_setting'            => true,
+            'edit_others_site_settings'      => true,
+            'delete_site_settings'           => true,
+            'publish_site_settings'          => true,
+            'publish_site_setting'           => true,
+            'read_private_site_settings'     => true,
+            'delete_private_site_settings'   => true,
+            'delete_published_site_settings' => true,
+            'delete_others_site_settings'    => true,
+            'edit_private_site_settings'     => true,
+            'edit_published_site_settings'   => true,
+            'edit_site_settings'             => true,
+
+            // Common
+            'unfiltered_html'                => true,
+        ];
+
+        $admin = get_role( 'administrator' );
+
+        if ( empty( $admin ) ) {
+            return;
         }
 
-        return $caps;
+        foreach ( $admin_rights as $cap => $grant ) {
+            $admin->add_cap( $cap, $grant );
+        }
+
+        unset( $admin );
     }
 }
