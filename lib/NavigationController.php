cab278100f4b663bd66f87771f1fb858b5c3d1b6
Ville Viklund <ville.viklund@geniem.com>
2021-06-16 14:41:33 +0300

    Hide custom links from navigation

--- lib/NavigationController.php
+++ lib/NavigationController.php
@@ -20,6 +20,11 @@ class NavigationController implements Interfaces\Controller {
             'after_setup_theme',
             \Closure::fromCallable( [ $this, 'register_nav_menus' ] )
         );
+
+        add_action(
+            'admin_head-nav-menus.php',
+            \Closure::fromCallable( [ $this, 'remove_custom_links' ] )
+        );
     }
 
     /**
@@ -28,5 +33,22 @@ class NavigationController implements Interfaces\Controller {
     protected function register_nav_menus() : void {
         register_nav_menu( 'primary', __( 'Primary Navigation', 'tms-theme-base' ) );
         register_nav_menu( 'secondary', __( 'Secondary Navigation', 'tms-theme-base' ) );
+    }
+
+    /**
+     * Remove nav menu meta-box links
+     */
+    function remove_custom_links() : void {
+        global $wp_meta_boxes;
+
+        if ( isset( $wp_meta_boxes['nav-menus'], $wp_meta_boxes['nav-menus']['side'] ) ) {
+            foreach ( $wp_meta_boxes['nav-menus']['side'] as $nav_menus ) {
+                foreach ( $nav_menus as $nav_id => $nav_menu ) {
+                    if ( $nav_id === 'add-custom-links' ) {
+                        remove_meta_box( $nav_id, 'nav-menus', 'side' );
+                    }
+                }
+            }
+        }
     }
 }
