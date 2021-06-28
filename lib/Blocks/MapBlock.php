e6575c373d40469a694759674bf7c5b5eed9e530
Ville Viklund <ville.viklund@geniem.com>
2021-06-28 15:31:43 +0300

    Add Map block

---
+++ lib/Blocks/MapBlock.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright (c) 2021. Geniem Oy
+ */
+
+namespace TMS\Theme\Base\Blocks;
+
+use Geniem\ACF\Block;
+use TMS\Theme\Base\ACF\Fields\ImageFields;
+use TMS\Theme\Base\ACF\Fields\MapFields;
+
+/**
+ * Class MapBlock
+ *
+ * @package TMS\Theme\Base\Blocks
+ */
+class MapBlock extends BaseBlock {
+
+    /**
+     * The block name (slug, not shown in admin).
+     *
+     * @var string
+     */
+    const NAME = 'map';
+
+    /**
+     * The block acf-key.
+     *
+     * @var string
+     */
+    const KEY = 'map';
+
+    /**
+     * The block icon
+     *
+     * @var string
+     */
+    protected $icon = 'location-alt';
+
+    /**
+     * Create the block and register it.
+     */
+    public function __construct() {
+        $this->title = 'Karttaupote';
+
+        parent::__construct();
+    }
+
+    /**
+     * Create block fields.
+     *
+     * @return array
+     */
+    protected function fields() : array {
+        $group = new MapFields( $this->title, self::NAME );
+
+        $group->remove_field( 'title' );
+        $group->remove_field( 'description' );
+
+        return apply_filters(
+            'tms/block/' . self::KEY . '/fields',
+            $group->get_fields()
+        );
+    }
+
+    /**
+     * This filters the block ACF data.
+     *
+     * @param array  $data       Block's ACF data.
+     * @param Block  $instance   The block instance.
+     * @param array  $block      The original ACF block array.
+     * @param string $content    The HTML content.
+     * @param bool   $is_preview A flag that shows if we're in preview.
+     * @param int    $post_id    The parent post's ID.
+     *
+     * @return array The block data.
+     */
+    public function filter_data( $data, $instance, $block, $content, $is_preview, $post_id ) : array {
+        $data = self::add_filter_attributes( $data, $instance, $block, $content, $is_preview, $post_id );
+
+        return apply_filters( 'tms/acf/block/' . self::KEY . '/data', $data );
+    }
+}
