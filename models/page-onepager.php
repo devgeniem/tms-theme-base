d06ed9bf8c9108888667e293bb4e5301894abc2b
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2021-06-29 11:00:41 +0300

    Add model & fields

---
+++ models/page-onepager.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (c) 2021. Geniem Oy
+ * Template Name: Onepager
+ */
+
+use TMS\Theme\Base\Traits\Components;
+
+/**
+ * The Onepager class.
+ */
+class PageOnepager extends BaseModel {
+
+    use Components;
+
+    /**
+     * Template
+     */
+    const TEMPLATE = 'models/page-onepager.php';
+}
