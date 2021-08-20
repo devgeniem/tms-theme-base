0a1f7a35e1e08b91106a506b14cef5cc2cbdb42b
Ville Viklund <ville.viklund@geniem.com>
2021-08-20 10:17:34 +0300

    Fix phpcs warnings

--- lib/ACF/Fields/Settings/SocialMediaSettingsTab.php
+++ lib/ACF/Fields/Settings/SocialMediaSettingsTab.php
@@ -44,7 +44,7 @@ class SocialMediaSettingsTab extends Tab {
      * @param null   $key   Key.
      * @param null   $name  Name.
      */
-    public function __construct( $label = '', $key = null, $name = null ) {
+    public function __construct( $label = '', $key = null, $name = null ) { // phpcs:ignore
         $label = $this->strings['tab'];
 
         parent::__construct( $label );
