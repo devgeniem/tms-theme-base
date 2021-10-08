a8d3705e9a2333da62c5a2369b4339ad03151aa3
Sami Harju <sami.harju@geniem.com>
2021-10-08 13:27:32 +0300

    Add links with icons in external links targeting

--- assets/scripts/external-links.js
+++ assets/scripts/external-links.js
@@ -22,6 +22,22 @@ export default class ExternalLinks {
         const domain = window.location.hostname;
         const icon = Common.makeIcon( 'external', 'icon--medium ml-1' );
 
+        // Links in regular context
         $( '#main-content a[href*="//"]:not(.button, .logo-wall__link, .link-list a, [href*="' + domain + '"])' ).append( icon ); // eslint-disable-line
+
+        // Links with icons (replace current icon with "opens in new window" -icon)
+        $( '#main-content a[href*="//"]:has(.icon):not(.link-list a, [href*="' + domain + '"])' ).each( function() {
+            const iconOld = $( this ).find( '.icon' );
+            const iconNew = $.parseHTML( icon );
+
+            // Copy the original icon classes to retain styling
+            $( iconNew ).addClass( iconOld.attr( 'class' ) );
+
+            // Remove the old icon
+            $( this ).find( '.icon' ).remove();
+
+            // Append new icon (links may have a child element like a span or p in which the icon is appended)
+            $( this ).children().first().append( iconNew );
+        } );
     }
 }
