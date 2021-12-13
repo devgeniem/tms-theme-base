de1adafd2a606380c3c175eb25e79e6424090034
Timo Haapala <timo.haapala@geniem.com>
2021-12-13 18:07:51 +0200

    cookiebot modal styles

--- lib/Cookiebot.php
+++ lib/Cookiebot.php
@@ -18,13 +18,13 @@ class Cookiebot implements Controller {
      * Hooks
      */
     public function hooks() : void {
-        add_filter( 'script_loader_tag', 
-        \Closure::fromCallable( [ $this, 'add_data_attribute' ] ),
+        add_filter( 'script_loader_tag',
+            \Closure::fromCallable( [ $this, 'add_data_attribute' ] ),
         10, 2 );
 
     }
 
-   /**
+    /**
      * Add data attribute data-cookieconsent="ignore" to
      * ignore script by cookiebot.
      * This prevents Cookiebot to block the scripts.
@@ -33,7 +33,6 @@ class Cookiebot implements Controller {
      * @param string $handle Script handle name.
      * @return string The script tag.
      */
-
     private function add_data_attribute( $tag, $handle ) {
         $scripts_to_ignore_by_cookiebot = [
             'jquery-core',
@@ -45,12 +44,12 @@ class Cookiebot implements Controller {
             'tms-plugin-materials-public-js',
             'vendor-js',
             'theme-js',
-            'ina-logout-js'
+            'ina-logout-js',
         ];
         if ( ! in_array( $handle, $scripts_to_ignore_by_cookiebot, true ) ) {
             return $tag;
         }
-       
+
         $tag = str_replace( '>', ' data-cookieconsent="ignore">', $tag );
         return $tag;
     }
