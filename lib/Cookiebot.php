29a385590b4da56508e22e35498b9a82703a07c9
Timo Haapala <timo.haapala@geniem.com>
2021-12-13 15:49:35 +0200

    Cookiebot script handling added

---
+++ lib/Cookiebot.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *  Copyright (c) 2021. Geniem Oy
+ */
+
+namespace TMS\Theme\Base;
+
+use TMS\Theme\Base\Interfaces\Controller;
+
+/**
+ * Class Cookiebot
+ *
+ * @package TMS\Theme\Base
+ */
+class Cookiebot implements Controller {
+
+    /**
+     * Hooks
+     */
+    public function hooks() : void {
+        add_filter( 'script_loader_tag', 
+        \Closure::fromCallable( [ $this, 'add_data_attribute' ] ),
+        10, 2 );
+
+    }
+
+   /**
+     * Add data attribute data-cookieconsent="ignore" to
+     * ignore script by cookiebot.
+     * This prevents Cookiebot to block the scripts.
+     *
+     * @param string $tag Script tag.
+     * @param string $handle Script handle name.
+     * @return string The script tag.
+     */
+
+    private function add_data_attribute( $tag, $handle ) {
+        var_dump( $handle );
+        $scripts_to_ignore_by_cookiebot = [
+            'theme-js',
+        ];
+        if ( ! in_array( $handle, $scripts_to_ignore_by_cookiebot, true ) ) {
+            return $tag;
+        }
+       
+        $tag = str_replace( '>', ' data-cookieconsent="ignore">', $tag );
+        return $tag;
+    }
+}
