a06a73942d97af71003db5031bf071bf9b624074
Mika <mika.miinalainen@geniem.com>
2022-02-22 09:55:32 +0200

    add ShareLinksLayout

--- lib/ACF/FrontPageGroup.php
+++ lib/ACF/FrontPageGroup.php
@@ -118,6 +118,7 @@ class PageFrontPageGroup {
                 Layouts\SubpageLayout::class,
                 Layouts\SocialMediaLayout::class,
                 Layouts\ContactsLayout::class,
+                Layouts\ShareLinksLayout::class,
             ],
             $key
         );
--- lib/ACF/PageGroup.php
+++ lib/ACF/PageGroup.php
@@ -168,6 +168,7 @@ class PageGroup {
                 Layouts\NoticeBannerLayout::class,
                 Layouts\GravityFormLayout::class,
                 Layouts\ContactsLayout::class,
+                Layouts\ShareLinksLayout::class,
             ],
             $key
         );
