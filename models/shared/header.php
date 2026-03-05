752897cf772b713ca83fce2968d4d7716d3bbc62
Erik Vänttinen <eepi1994@hotmail.com>
2026-03-05 10:37:48 +0200

    TMS-1237: Add a custom Google Translate functionality to navigation

--- models/shared/header.php
+++ models/shared/header.php
@@ -19,7 +19,7 @@ class Header extends Model {
      * Hooks.
      */
     public function hooks() {
-        add_filter(
+        \add_filter(
             'dustpress/menu/item/classes',
             Closure::fromCallable( [ $this, 'menu_item_classes' ] ),
             10,
@@ -445,5 +445,25 @@ class Header extends Model {
      */
     public function strings() {
         return ( new Strings() )->s()['header'];
+    }
+
+    /**
+     * Get gtranslate dropdown menu
+     *
+     * @return array|null
+     */
+    public function gtranslate_menu() : ?array {
+        // Check if the menu is enabled in site settings
+        if ( ! Settings::get_setting( 'enable_gtranslate_menu' ) ) {
+            return null;
+        }
+
+        $current_lang = DPT_PLL_ACTIVE ? \pll_current_language() : \get_locale();
+
+        return [
+            'current_language'          => $current_lang,
+            'select_text'               => \__( 'Select language', 'tms-theme-base' ),
+            'disclaimer_text'           => \__( 'The City of Tampere is not responsible for translations made by Google Translate.', 'tms-theme-base' ),
+        ];
     }
 }
